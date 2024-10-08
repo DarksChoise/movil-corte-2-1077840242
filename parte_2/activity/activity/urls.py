7c4adaa9c86d4444b9f6a734bbf80799e91151ac
DarksChoice <andradecardozojd@gmail.com>
2024-10-08 16:40:30 -0500

    feat(parte_2): Finist Part 2

--- parte_2/activity/activity/urls.py
+++ parte_2/activity/activity/urls.py
@@ -2,9 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 
 from rest_framework import permissions
+from rest_framework import routers
+
+from apps.usuarios.views.usuario_view import UsuarioViewSet
+from apps.actividades.views.actividad_view import ActividadView
 
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
+
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -20,11 +25,14 @@ schema_view = get_schema_view(
     permission_classes=(permissions.AllowAny,),
 )
 
+router = routers.DefaultRouter()
+
+router.register(r'usuario', UsuarioViewSet, basename='Usuario')
+router.register(r'actividad', ActividadView, basename='Actividad')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    path('', include('apps.usuarios.urls')),
-    path('', include('apps.actividades.urls')),
+    path('', include(router.urls)),
 
 ]
